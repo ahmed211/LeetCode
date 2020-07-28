efd5b1577ba6e41dfe298bfe7c0f9ae28ef4648c
ahmed211 <ahmed.cs_11299@hotmail.com>
2020-07-28 22:47:37 +0200

    First Unique Character in a String

---
+++ src/proplems_easy/387. First Unique Character in a String.kt
@@ -0,0 +1,20 @@
+package proplems_easy
+
+fun firstUniqChar(s: String): Int {
+    var alphabet = IntArray(26) {0}
+
+    for (char in s){
+        alphabet[char.toInt()%26]++
+    }
+
+    for (i in s.indices){
+        if( alphabet[s[i].toInt()%26] == 1){
+            return i
+        }
+    }
+    return -1
+}
+
+fun main(){
+    print(firstUniqChar("loveleetcode"))
+}

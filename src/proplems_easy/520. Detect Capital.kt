d0c2e52e5a1c83514c92192e78838cafe1d919de
ahmed211 <ahmed.cs_11299@hotmail.com>
2020-08-04 01:00:57 +0200

    detectCapitalUse

---
+++ src/proplems_easy/520. Detect Capital.kt
@@ -0,0 +1,20 @@
+package proplems_easy
+
+fun detectCapitalUse(word: String): Boolean {
+    var capital = 0
+    var lower = 0
+    for (char in word){
+        if (char.isLowerCase()){
+            lower++
+        }
+        else{
+            capital++
+        }
+    }
+    return capital == word.length || lower == word.length || (capital == 1 && word[0].isUpperCase())
+}
+
+
+fun main(){
+    print(detectCapitalUse("FlaG"))
+}

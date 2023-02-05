23b635b54c10828b2641b9fee9d294f9a7da0983
Ahmed Mostafa <amostafa@hala.com>
2023-02-05 13:02:14 +0200

    1480. Running Sum of 1d Array

--- src/problems_easy/1480. Running Sum of 1d Array.kt
+++ src/problems_easy/1480. Running Sum of 1d Array.kt
@@ -1,14 +1,14 @@
 package problems_easy
 
+
 fun runningSum(nums: IntArray): IntArray {
-    for (i in nums.indices){
-        if (i != 0) {
-            nums[i] = nums[i - 1] + nums[i]
-        }
+
+    for (i in 1 until nums.size){
+        nums[i] += nums[i-1]
     }
     return nums
 }
 
-fun main(){
-    println(runningSum(intArrayOf(1,1,1,1,1)).contentToString())
+fun main() {
+    println( runningSum(intArrayOf(1, 2,3)).contentToString())
 }
